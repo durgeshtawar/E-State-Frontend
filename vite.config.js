af93626dc61595c251721bc3081eec8ac1d0102e
Durgesh Tawar <adityatawar58@gmail.com>
2024-05-21 17:50:43 +0530

    add vite config more info

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  root: './',
   plugins: [react()],
   build: {
-    outDir: 'dist'
+    outDir: 'dist',
+    rollupOptions: {
+      input: 'index.html' 
+    }
   }
 })
