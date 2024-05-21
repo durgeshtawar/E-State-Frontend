18f13f8204975a73c65206709bd1cf27af4fbf06
Durgesh Tawar <adityatawar58@gmail.com>
2024-05-21 18:12:46 +0530

    add root directory

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  root: './',
+  root: './index.html',
   plugins: [react()],
   build: {
     outDir: 'dist',
