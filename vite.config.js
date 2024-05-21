81f46c6bc1e7ed4d75190c93ce59ad8e3c4205df
Durgesh Tawar <adityatawar58@gmail.com>
2024-05-21 18:17:44 +0530

    fix the bugs

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  root: './index.html',
+  root: './dist',
   plugins: [react()],
   build: {
     outDir: 'dist',
