1f5d9fc84fd9a0620617a626a6b088e2d0d49fd0
Durgesh Tawar <adityatawar58@gmail.com>
2024-05-22 11:37:19 +0530

    add css in register page

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom"
-// import "../styles/Register.scss";
+import "../styles/Register.scss";
 import { Link } from "react-router-dom";
 
 const RegisterPage = () => {
